b6442097cbd41b101eaf68bc4e747422d1c6145c
john longanecker <johnlonganecker@gmail.com>
2025-02-16 09:04:12 -0800

    add in a few more useful mini plugins

--- init.lua
+++ init.lua
@@ -900,6 +900,9 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
+      require('mini.indentcope').setup { delay = 100 }
+      require('mini.tralspace').setup()
+
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin

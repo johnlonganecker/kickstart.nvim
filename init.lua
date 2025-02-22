149094a604f582c2ad7e8ba6c4bab8e3d1c407bc
john longanecker <johnlonganecker@gmail.com>
2025-02-22 09:59:15 -0800

    add some more plugins

--- init.lua
+++ init.lua
@@ -964,6 +964,8 @@ require('lazy').setup({
   -- require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.dadbod-ui',
+  require 'kickstart.plugins.flash',
+  -- require 'kickstart.plugins.avante',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.

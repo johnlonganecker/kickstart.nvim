f2b3be70c2565f9a0275bac695baaa426e91a129
john longanecker <johnlonganecker@gmail.com>
2025-03-14 23:06:59 -0700

    configure fo claude

--- lua/kickstart/plugins/avante.lua
+++ lua/kickstart/plugins/avante.lua
@@ -1,24 +1,23 @@
 return {
   'yetone/avante.nvim',
   event = 'VeryLazy',
-  lazy = false,
   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   opts = {
     -- add any opts here
     -- for example
-    provider = 'openai',
+    provider = 'claude',
     openai = {
-      endpoint = 'https://api.openai.com/v1',
-      model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
-      timeout = 30000, -- timeout in milliseconds
-      temperature = 0, -- adjust if needed
+      endpoint = 'https://api.anthropic.com',
+      model = 'claude-3-5-sonnet-20241022',
+      temperature = 0,
       max_tokens = 4096,
-      reasoning_effort = 'high', -- only supported for "o" models
     },
   },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = 'make',
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
+    'nvim-treesitter/nvim-treesitter',
     'stevearc/dressing.nvim',
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',

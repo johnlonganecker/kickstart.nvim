98617d161c6b0d30a5bdd950f1b1660d14276a28
Yassine Zouggari <git@zoug.fr>
2026-01-27 14:19:51 +0000

    update: remove client_supports_method (#1810)
    Neovim 0.11 is now stable

--- init.lua
+++ init.lua
@@ -827,25 +827,13 @@ require('lazy').setup({
           map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 >>>>>>> 1a5787b (Change LSP Keybindings to Match the Default `gr` Bindings Introduced in Neovim 0.11 (#1427))
 
-          -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-          ---@param client vim.lsp.Client
-          ---@param method vim.lsp.protocol.Method
-          ---@param bufnr? integer some lsp support methods only in specific files
-          ---@return boolean
-          local function client_supports_method(client, method, bufnr)
-            if vim.fn.has 'nvim-0.11' == 1 then
-              return client:supports_method(method, bufnr)
-            else
-              return client.supports_method(method, { bufnr = bufnr })
-            end
-          end
-
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
           --
           -- When you move your cursor, the highlights will be cleared (the second autocommand).
           local client = vim.lsp.get_client_by_id(event.data.client_id)
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
           if client and client:supports_method('textDocument/documentHighlight', event.buf) then
@@ -859,6 +847,11 @@ require('lazy').setup({
 =======
           if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
 >>>>>>> e64aa51 (fix: regression introduced in db78c0b217fd9525e2cbcbffd18abbbbddc75b2e (#1367))
+||||||| parent of 3ddda4a (update: remove client_supports_method (#1810))
+          if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+=======
+          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+>>>>>>> 3ddda4a (update: remove client_supports_method (#1810))
             local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
               buffer = event.buf,
@@ -887,10 +880,16 @@ require('lazy').setup({
           -- This may be unwanted, since they displace some of your code
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
           if client and client:supports_method('textDocument/inlayHint', event.buf) then
             map('<leader>th', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }) end, '[T]oggle Inlay [H]ints')
 ||||||| parent of db78c0b (fix: arguments for the `vim.lsp.Client.supports_method` method (#1356))
           if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+||||||| parent of 3ddda4a (update: remove client_supports_method (#1810))
+          if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+=======
+          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+>>>>>>> 3ddda4a (update: remove client_supports_method (#1810))
             map('<leader>th', function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
             end, '[T]oggle Inlay [H]ints')
